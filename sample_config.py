6db628607a82d6b45e6b091e73cd8cddfaa3f7a1
Bedang Sen <35425072+bedangSen@users.noreply.github.com>
2019-04-03 13:55:23 +0400

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,11 +1,3 @@
 # recognize speech using IBM Speech to Text
-IBM_USERNAME = ""  # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-IBM_PASSWORD = ""  # IBM Speech to Text passwords are mixed-case alphanumeric strings
-
-# recognize speech using Google Speech to Text
-GOOGLE_USERNAME=""
-GOOGLE_PASSWORD=""
-
-# recognize speech using Microsoft Speech to Text
-MICROSOFT_USERNAME=""
-MICROSOFT_PASSWORD=""
+IAM_APIKEY = "IAM_APIKEY"  
+URL = "URL" 
